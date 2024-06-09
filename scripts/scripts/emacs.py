fc3c38433708a080b5aa0fe3da3878ee2c9b8667
Jamsjz <gj984233@gmail.com>
2024-06-10 01:58:22 +0545

    first commit

---
+++ scripts/scripts/emacs.py
@@ -0,0 +1,43 @@
+import sys
+import subprocess as sp
+
+
+def usage():
+    print(
+        """
+    Usage:
+        emacs:
+            start
+            kill
+        eclient:
+            start
+            restart"""
+    )
+
+
+args = sys.argv
+i = 1
+if len(sys.argv) > 1:
+    if args[i] == "emacs":
+        if args[i + 1] == "start":
+            sp.Popen("emacsclient -c -a 'emacs'", shell=True)
+        else:
+            usage()
+    elif args[i] == "eclient":
+        if args[i + 1] == "start":
+            sp.Popen("/usr/bin/emacs --daemon &", shell=True)
+        elif args[i + 1] == "kill":
+            sp.Popen("emacsclient -e '(kill-emacs)'", shell=True)
+        elif args[i + 1] == "restart":
+            sp.Popen(
+                "emacsclient -e '(kill-emacs)'; /usr/bin/emacs --daemon &",
+                shell=True,
+            )
+        else:
+            usage()
+    else:
+        usage()
+
+
+else:
+    usage()
